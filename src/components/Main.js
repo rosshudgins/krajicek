d6d8f1fa89c9297df158344f6c51c6bc73e5aabe
Ross Hudgins <michael.ross.hudgins@gmail.com>
2019-05-04 01:20:14 +0000

    add a honypot

--- src/components/Main.js
+++ src/components/Main.js
@@ -41,6 +41,9 @@ class Main extends React.Component {
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
           <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+            <p class="hidden">
+              <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+            </p>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
