cde028d2a07d6abe62447f3f493076c300057990
Ross Hudgins <michael.ross.hudgins@gmail.com>
2019-05-04 01:25:46 +0000

    remove contact form, just do info

--- src/components/Main.js
+++ src/components/Main.js
@@ -40,28 +40,8 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-            <p class="hidden">
-              <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-            </p>
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <div data-netlify-recaptcha="true"></div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
+          <p>Call me at a number: </p>
+          <p>Email me at: </p>
           <ul className="icons">
             <li><a href="https://www.facebook.com/John-Krajicek-Borderline-Social-Club-195045807745923/" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
             <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
