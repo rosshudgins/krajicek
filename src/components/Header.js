23697d1235caaf11ab0df36ce46a07132e46c458
Ross Hudgins <michael.ross.hudgins@gmail.com>
2019-05-05 00:47:21 +0000

    fixed button displays

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const ButtonStyleOverride = {
+  boxShadow: "none",
+}
+
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
@@ -14,10 +18,10 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><button onClick={() => {props.onOpenArticle('about')}}>About</button></li>
-                <li><button onClick={() => {props.onOpenArticle('music')}}>Music</button></li>
-                <li><button onClick={() => {props.onOpenArticle('painting')}}>Painting</button></li>
-                <li><button onClick={() => {props.onOpenArticle('contact')}}>Contact</button></li>
+                <li><button style={ButtonStyleOverride} onClick={() => {props.onOpenArticle('about')}}>About</button></li>
+                <li><button style={ButtonStyleOverride} onClick={() => {props.onOpenArticle('music')}}>Music</button></li>
+                <li><button style={ButtonStyleOverride} onClick={() => {props.onOpenArticle('painting')}}>Painting</button></li>
+                <li><button style={ButtonStyleOverride} onClick={() => {props.onOpenArticle('contact')}}>Contact</button></li>
             </ul>
         </nav>
     </header>
